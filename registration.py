673d2f6dc7421fbb2f784846d9c2b9ea06cd685c
aswin <aswinsynefo3132001@gmail.com>
2023-05-19 13:28:39 +0530

    marksheet add

--- registration.py
+++ registration.py
@@ -13,6 +13,7 @@ a.place(x=200,y=30)
 b=Label(text='username')
 b.pack()
 b.place(x=30,y=100)
+
 e=Entry(width=30)
 e.pack()
 e.place(x=150,y=100)
@@ -20,6 +21,7 @@ e.place(x=150,y=100)
 c=Label(text='Password')
 c.pack()
 c.place(x=30,y=150)
+
 f=Entry(width=30)
 f.pack()
 f.place(x=150,y=150)
@@ -27,11 +29,24 @@ f.place(x=150,y=150)
 d=Label(text='DOB')
 d.pack()
 d.place(x=30,y=200)
+
 g=Entry(width=30)
 g.pack()
 g.place(x=150,y=200)
 
-x=Button(text='submit',padx=10,pady=5,bg='black',fg='white')
+def display():
+    la = Label(text="welcome "+e.get())
+    la.pack()
+    la.place(x=180,y=450)
+    l=Label(text="your password is "+f.get())
+    l.pack()
+    l.place(x=180,y=500)
+    lab=Label(text="your DOB is "+g.get())
+    lab.pack()
+    lab.place(x=180,y=550) 
+
+
+x=Button(text='submit',command=display,padx=10,pady=5,bg='black',fg='white')
 x.pack()
 x.place(x=200,y=300)
 
